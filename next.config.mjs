9089fea592eb4ffbad0f1e0f82e067bb3c9ae72d
Brady DeBoer <thebradster7.bd@gmail.com>
2024-11-12 15:15:39 -0500

    Webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
+  webpack(config, { isServer }) {
     config.module.rules.push({
       test: /\.svg$/,
       use: "@svgr/webpack",
     })
+    if (!isServer) {
+      config.externals.push("@sparticuz/chromium")
+    }
     return config
   },
 }
