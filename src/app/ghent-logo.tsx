d101e315fb0c29bf7ffb0dcd8478f23286ad4c37
Brady DeBoer <bradydeboer195@gmail.com>
2025-03-14 11:59:27 -0400

    Fixed mismatched server client error

--- src/app/ghent-logo.tsx
+++ src/app/ghent-logo.tsx
@@ -3,13 +3,20 @@
 import Ghent from "@/assets/img/ghent.svg"
 import GhentDark from "@/assets/img/ghent-dark.svg"
 import { useTheme } from "next-themes"
+import { useEffect, useState } from "react"
 
 export function GhentLogo({ className }: { className: string }) {
-  "use client"
+  const { resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false)
 
-  const { resolvedTheme } = useTheme()
+  useEffect(() => {
+    setMounted(true)
+  }, []);
 
-  const Theme = resolvedTheme === "dark" ? GhentDark : Ghent
+  const Theme =
+    mounted && resolvedTheme === "dark"
+      ? GhentDark
+      : Ghent
 
   return <Theme className={className} />
 }
