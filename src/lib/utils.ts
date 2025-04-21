360e42da490661e75ca64655b12ea9ee4f3c4a31
Brady DeBoer <bradydeboer195@gmail.com>
2025-04-21 09:26:07 -0400

    updated IM pricing

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,32 +8,29 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getAmount(player: Player) {
-  return (
-    120 +
-    Number(player.isPlayingUp) * 20.0 -
-    Math.min(
-      Number(player.isFemale) * 10.0 +
-        Number(player.ageRange !== "18to64") * 10.0 +
-        Number(
-          player.FIDETitle != null &&
-            player.FIDETitle.includes("FIDE Master") &&
-            !player.FIDETitle.includes("Woman FIDE") &&
-            !player.FIDETitle.includes("Arena")
-        ) *
-          10.0,
-      20.0
-    )
-  )
+  return (player.FIDETitle != null &&
+    formatFIDETitle(player.FIDETitle).includes("IM") &&
+    !player.FIDETitle.includes("Arena")) ||
+    (player.FIDERating != null && player.FIDERating >= 2400)
+    ? 60
+    : 120 +
+        Number(player.isPlayingUp) * 20.0 -
+        Math.min(
+          Number(player.isFemale) * 10.0 +
+            Number(player.ageRange !== "18to64") * 10.0 +
+            Number(
+              player.FIDETitle != null &&
+                player.FIDETitle.includes("FIDE Master") &&
+                !player.FIDETitle.includes("Woman FIDE") &&
+                !player.FIDETitle.includes("Arena")
+            ) *
+              10.0,
+          20.0
+        )
 }
 
 export function getsFreeEntry(player: Player) {
-  return (
-    (player.FIDETitle != null &&
-      (player.FIDETitle.includes("Grandmaster") ||
-        formatFIDETitle(player.FIDETitle).includes("IM")) &&
-      !player.FIDETitle.includes("Arena")) ||
-    (player.FIDERating != null && player.FIDERating >= 2400)
-  )
+  return player.FIDETitle != null && player.FIDETitle.includes("Grandmaster")
 }
 
 export function parseFEN(fen: string) {
