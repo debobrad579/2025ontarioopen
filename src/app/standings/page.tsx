02cf597f6b956fb40bada7d422c0cd4e1d03d28b
Brady DeBoer <bradydeboer195@gmail.com>
2025-05-20 11:36:35 -0400

    Changes to standings page

--- src/app/standings/page.tsx
+++ src/app/standings/page.tsx
@@ -1,6 +1,9 @@
+import { geistMono } from "@/assets/fonts/fonts";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { getPairings } from "@/db/select";
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -16,7 +19,7 @@ export default async function Standings() {
         <TabsTrigger value="open">Open</TabsTrigger>
         <TabsTrigger value="u2000">U2000</TabsTrigger>
         <TabsTrigger value="u1600">U1600</TabsTrigger>
-        <TabsTrigger value="u1200">U1200/Unr.</TabsTrigger>
+        <TabsTrigger value="u1200">U1200</TabsTrigger>
       </TabsList>
       <TabsContent value="open">
         <PairingsTable standings={open.pairings} title={open.title} />
@@ -41,38 +44,41 @@ function PairingsTable({ standings, title }: { standings: { [key: string]: strin
   return (
     <div className="space-y-4">
       <h1 className="font-bold text-xl text-center">{title}</h1>
-      <Table className="mb-4">
-        <TableHeader>
-          <TableRow>
-            <TableHead>Place</TableHead>
-            <TableHead>Name</TableHead>
-            <TableHead>Rating</TableHead>
-            <TableHead>Rd 1</TableHead>
-            <TableHead>Rd 2</TableHead>
-            <TableHead>Rd 3</TableHead>
-            <TableHead>Rd 4</TableHead>
-            <TableHead>Rd 5</TableHead>
-            <TableHead>Rd 6</TableHead>
-            <TableHead>Total</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {standings.map(standing => (
-            <TableRow key={standing.n}>
-              <TableCell>{standing.place}</TableCell>
-              <TableCell>{standing.name}</TableCell>
-              <TableCell>{standing.rating}</TableCell>
-              <TableCell>{standing.rd1}</TableCell>
-              <TableCell>{standing.rd2}</TableCell>
-              <TableCell>{standing.rd3}</TableCell>
-              <TableCell>{standing.rd4}</TableCell>
-              <TableCell>{standing.rd5}</TableCell>
-              <TableCell>{standing.rd6}</TableCell>
-              <TableCell>{standing.total}</TableCell>
+      <ScrollArea>
+        <Table className={cn("mb-4", geistMono.className)}>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Place</TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead>Rating</TableHead>
+              <TableHead>Rd 1</TableHead>
+              <TableHead>Rd 2</TableHead>
+              <TableHead>Rd 3</TableHead>
+              <TableHead>Rd 4</TableHead>
+              <TableHead>Rd 5</TableHead>
+              <TableHead>Rd 6</TableHead>
+              <TableHead>Total</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {standings.map(standing => (
+              <TableRow key={standing.n}>
+                <TableCell>{standing.place}</TableCell>
+                <TableCell>{standing.name}</TableCell>
+                <TableCell>{standing.rating}</TableCell>
+                <TableCell>{standing.rd1.replace("---", "--")}</TableCell>
+                <TableCell>{standing.rd2.replace("---", "--")}</TableCell>
+                <TableCell>{standing.rd3.replace("---", "--")}</TableCell>
+                <TableCell>{standing.rd4.replace("---", "--")}</TableCell>
+                <TableCell>{standing.rd5.replace("---", "--")}</TableCell>
+                <TableCell>{standing.rd6.replace("---", "--")}</TableCell>
+                <TableCell>{standing.total}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
     </div>
-     )
+  )
 }
